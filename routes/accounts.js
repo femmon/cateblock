0b5e64fc0198cb6cf56cf0b25976e626ecbcf483
femmon <49613439+femmon@users.noreply.github.com>
2019-01-29 11:45:48 +0700

    Minor fix

--- routes/accounts.js
+++ routes/accounts.js
@@ -34,7 +34,7 @@ router.post("/signup", async (req, res) => {
         let username = req.body.username;
         let password = req.body.password;
         if (isUsernameValid(username) && password.length > 5) {
-            let connection = await pool.getConnection;
+            let connection = await pool.getConnection();
             let results = await pool.query("SELECT COUNT(*) FROM Accounts WHERE Username = ?;", [username], connection);
             if (!results[0]["COUNT(*)"]) {
                 let salt = crypto.randomBytes(128).toString('base64');
