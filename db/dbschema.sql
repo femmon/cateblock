5e94e8caecfc3a35f6a58ba58eb3f4e28f5e38b1
femmon <49613439+femmon@users.noreply.github.com>
2019-02-04 19:22:45 +0700

    Minor fix

--- db/dbschema.sql
+++ db/dbschema.sql
@@ -15,6 +15,8 @@ CREATE TABLE Entries (
         ON DELETE CASCADE
 );
 
+-- Time works on the assumption that database timezone is in UTC
+
 CREATE TABLE History (
     EntryID INT,
     Content TEXT,
@@ -23,6 +25,11 @@ CREATE TABLE History (
     FOREIGN KEY (EntryID)
         REFERENCES Entries(EntryID)
         ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 -- If select post from table then sort with PostTime, there is a small chance that the time will collide. Generations remove that chance
+
+-- Use these to remove hole in auto increment entry ID (Use along with ON UPDATE CASCADE):
+-- SET @count = 0;
+-- UPDATE Entries SET EntryID = @count:= @count + 1 WHERE EntryID <> 0;
