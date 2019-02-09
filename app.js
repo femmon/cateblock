5884d292f35678126ee60dd8f5110c4dfa5eae72
femmon <49613439+femmon@users.noreply.github.com>
2019-02-09 23:34:43 +0700

    Session works: timer reset after new request

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(session({
         password : layout[2],
         port     : layout[4],
     }),
-    // cookie: { maxAge: 60 * 60 * 1000 }
-    cookie: { maxAge: 1 * 60 * 1000 }
+    cookie: { maxAge: 60 * 60 * 1000 }
 }));
 
 app.use(express.json());
