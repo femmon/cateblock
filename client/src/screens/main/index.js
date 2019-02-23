703a45b8a09a5545f6ea79f5fa34bff8ca564aeb
femmon <49613439+femmon@users.noreply.github.com>
2019-02-23 09:36:53 +0700

    Add delete account

--- client/src/screens/main/index.js
+++ client/src/screens/main/index.js
@@ -5,12 +5,33 @@ import LogoutButton from "./components/logout-button";
 import Content from "./components/content";
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClickDeleteAccount = this.handleClickDeleteAccount.bind(this);
+    }
+    handleClickDeleteAccount() {
+        fetch("/delete-account", {method: "POST"}).then(res => {
+            if (res.status === 200) {
+                this.props.handleClickStatus("logout");
+            }
+        }).catch(err => {throw err});
+    }
     render() {
         return (
             <React.Fragment>
                 <Header />
                 {this.props.status === "try" && <Form handleClickStatus={(status) => this.props.handleClickStatus(status)} />}
-                {this.props.status === "login" && <LogoutButton handleClickStatus={(status) => this.props.handleClickStatus(status)} />}
+                {this.props.status === "login" && (
+                    <React.Fragment>
+                        <div onClick={this.handleClickDeleteAccount}>Delete Account</div>
+                        <LogoutButton handleClickStatus={(status) => this.props.handleClickStatus(status)} />
+                    </React.Fragment>
+                )}
+                {/* function returnAccount() {
+                    let logoutButton = <LogoutButton handleClickStatus={(status) => this.props.handleClickStatus(status)} />
+                    return <Hamburger child={[accountSetting, logoutButton]}>
+                } */}
+                {/* {this.props.status === "login" && returnAccount()} */}
                 <Content status={this.props.status} />
             </React.Fragment>
         );
