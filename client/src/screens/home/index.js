393ab961e8eb6d03386b97b6364406c9538fbde4
femmon <49613439+femmon@users.noreply.github.com>
2019-06-30 13:53:53 +0700

    Reformat, add semantic elements and divs to Home

--- client/src/screens/home/index.js
+++ client/src/screens/home/index.js
@@ -4,22 +4,30 @@ import Button from "../../components/button";
 import FormContainer from "../../components/form-container";
 
 function Home(props) {
-    return (
-        <React.Fragment>
+    return (<React.Fragment>
+        <header>
             <Logo />
             <h2>What good thing happened today?</h2>
-            {props.status === "logout" && (
-                <React.Fragment>
+        </header>
+
+        {props.status === "logout" && (
+            <main>
+                <div>
                     <Button
                         className="tryButton"
                         content="Write it down!"
                         onClick={() => props.handleStateTry()}
                     />
-                    <FormContainer handleStateLogin={() => props.handleStateLogin()} />
-                </React.Fragment>
-            )}
-        </React.Fragment>
-    );
+                </div>
+
+                <div>
+                    <FormContainer
+                        handleStateLogin={() => props.handleStateLogin()}
+                    />
+                </div>
+            </main>
+        )}
+    </React.Fragment>);
 }
 
 export default Home;
