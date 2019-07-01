f30540dfd7b4c1d542cc3305aa3f617b6472d6d6
femmon <49613439+femmon@users.noreply.github.com>
2019-07-01 21:56:29 +0700

    Reformat, use semantic elements in Stack

--- client/src/screens/main/components/content/components/stack/index.js
+++ client/src/screens/main/components/content/components/stack/index.js
@@ -25,44 +25,46 @@ class Stack extends React.Component {
         })
     }
     render() {
-        return (
-            <div>
-                {this.props.posts.map(({EntryID, Content, PostTime, Edited}) => {
-                    return (
-                        <div key={EntryID} style={{border: "5px solid black"}}>
-                            <div>
-                                <p>{toLocalePostTime(PostTime)}</p>
-                                {Edited !== 0 && <p>Edited</p>}
-                                <Hamburger
-                                    isOpeningHistory={this.state.history === EntryID}
-                                    closeViewEdit={this.closeViewEdit}
-                                    handleClickEditorEdit={() => {
-                                        this.props.handleClickEditorEdit(EntryID);
-                                    }}
-                                    Edited={Edited}
-                                    viewEdit={() => this.viewEdit(EntryID)}
-                                    postDelete={() => this.props.postDelete(EntryID)}
-                                />
-                            </div>
-                            <p>{Content}</p>
-                            {this.state.history === EntryID && (
-                                <div style={{border: "5px solid black"}}>
-                                    <span onClick={() => this.closeViewEdit()}>Close</span>
-                                    {this.state.posts.map(({Content, PostTime}, index) => {
-                                        return (
-                                            <div key={index} style={{border: "5px solid black"}}>
-                                                <p>{toLocalePostTime(PostTime)}</p>
-                                                <p>{Content}</p>
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                            )}
+        return (<section>
+            {this.props.posts.map(({EntryID, Content, PostTime, Edited}) => {
+                return (<article key={EntryID} style={{border: "5px solid black"}}>
+                    <div>
+                        <p>{toLocalePostTime(PostTime)}</p>
+
+                        {Edited !== 0 && <p>Edited</p>}
+
+                        <Hamburger
+                            isOpeningHistory={this.state.history === EntryID}
+                            closeViewEdit={this.closeViewEdit}
+                            handleClickEditorEdit={() => {
+                                this.props.handleClickEditorEdit(EntryID);
+                            }}
+                            Edited={Edited}
+                            viewEdit={() => this.viewEdit(EntryID)}
+                            postDelete={() => this.props.postDelete(EntryID)}
+                        />
+                    </div>
+
+                    <p>{Content}</p>
+
+                    {this.state.history === EntryID && (
+                        <div style={{border: "5px solid black"}}>
+                            <span onClick={() => this.closeViewEdit()}>Close</span>
+
+                            {this.state.posts.map(({Content, PostTime}, index) => {
+                                return (
+                                    <div key={index} style={{border: "5px solid black"}}>
+                                        <p>{toLocalePostTime(PostTime)}</p>
+
+                                        <p>{Content}</p>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    );
-                })}
-            </div>
-        );
+                    )}
+                </article>);
+            })}
+        </section>);
     }
 }
 
