3dd7c04604b12dcf8dc3239a2178cf4145d5dfa7
femmon <49613439+femmon@users.noreply.github.com>
2019-07-13 23:47:00 +0700

    Add GlobalStyle

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import Home from "./screens/home";
 import Main from "./screens/main";
 import "normalize.css";
 import "./style.css";
+import GlobalStyle from "./global-style";
 
 class App extends React.Component {
     constructor(props) {
@@ -29,42 +30,45 @@ class App extends React.Component {
         return this.checkSession();
     }
     render() {
+        let Screen;
+
         switch (this.state.status) {
             case "lost":
-                return (
-                    <div>
-                        <h1>404</h1>
-                        <p>Hey there buddy. Are you lost</p>
-                        <button onClick={() => {
-                            window.history.replaceState({}, "", "/");
-                            this.checkSession();
-                        }}>Go to home page</button>
-                    </div>
-                );
+                Screen = <div>
+                    <h1>404</h1>
+                    <p>Hey there buddy. Are you lost</p>
+                    <button onClick={() => {
+                        window.history.replaceState({}, "", "/");
+                        this.checkSession();
+                    }}>Go to home page</button>
+                </div>;
+                break;
 
             case null:
             case "logout":
-                return (
-                    <Home
-                        status={this.state.status}
-                        handleStateLogin={() => this.setState({status: "login"})}
-                        handleStateTry={() => this.setState({status: "try"})}
-                    />
-                );
+                Screen = <Home
+                    status={this.state.status}
+                    handleStateLogin={() => this.setState({status: "login"})}
+                    handleStateTry={() => this.setState({status: "try"})}
+                />;
+                break;
 
             case "login":
             case "try":
-                return (
-                    <Main
-                        status={this.state.status}
-                        handleStateLogin={() => this.setState({status: "login"})}
-                        handleStateLogout={() => this.setState({status: "logout"})}
-                    />
-                );
+                Screen = <Main
+                    status={this.state.status}
+                    handleStateLogin={() => this.setState({status: "login"})}
+                    handleStateLogout={() => this.setState({status: "logout"})}
+                />;
+                break;
 
             default:
                 throw new Error("Not supported status");
         }
+        return (<React.Fragment>
+            <GlobalStyle />
+            {Screen}
+        </React.Fragment>);
     }
 }
 
