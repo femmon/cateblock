3bb19e89c74760e64023bdb8eb6ac6e457f35e1c
femmon <49613439+femmon@users.noreply.github.com>
2019-07-16 10:34:37 +0700

    Set Babel env at the right place

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,5 +1,8 @@
 const common = require("./webpack.common.js");
 
+//This is to set env value of Babel
+process.env.NODE_ENV = "production";
+
 module.exports = Object.assign({
     mode: "production",
 }, common);
