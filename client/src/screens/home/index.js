ce7e190a5f97ea3993091326835ca36d55d7a43c
femmon <49613439+femmon@users.noreply.github.com>
2019-07-18 20:36:05 +0700

    Add css to Home: divide into 2 columns, transition when leaving from Home

--- client/src/screens/home/index.js
+++ client/src/screens/home/index.js
@@ -1,30 +1,84 @@
 import React from "react";
+import styled from "styled-components";
 import Logo from "../../components/logo";
 import Button from "../../components/button";
 import FormContainer from "../../components/form-container";
 
+let transitionTime = "1s";
+
+const Header = styled.header`
+    text-align: center;
+
+    opacity: 1;
+    transition: ${transitionTime} ease-in;
+`;
+
+const MainContainer = styled.main`
+    display: flex;
+    justify-content: center;
+
+    overflow-x: hidden;
+`;
+
+const LeftBox = styled.div`
+    flex: 1;
+
+    position: relative;
+    right: 0;
+    opacity: 1;
+    transition: ${transitionTime} ease-in;
+    button {
+        float: right;
+    }
+`;
+
+const RightBox = styled.div`
+    flex: 1;
+
+    position: relative;
+    left: 0;
+    opacity: 1;
+    transition: ${transitionTime} ease-in;`;
+
 function Home(props) {
+    function changeScreenAnimation() {
+        return new Promise(res => {
+            let head = document.querySelector("header");
+            head.style.opacity = 0.25;
+
+            let sides = document.querySelectorAll("main > div");
+            sides[0].style.right = "25%";
+            sides[0].style.opacity = 0.25;
+            sides[1].style.left = "25%";
+            sides[1].style.opacity = 0.25;
+            setTimeout(res, Number(transitionTime.replace("s", "")) * 1000);
+        });
+    }
     return (<React.Fragment>
-        <header css={`text-align: center`}>
+        <Header>
             <Logo />
             <h2>What good thing happened today?</h2>
-        </header>
+        </Header>
 
         {props.status === "logout" && (
-            <main>
-                <div>
+            <MainContainer>
+                <LeftBox>
                     <Button
-                        onClick={() => props.handleStateTry()}
+                        onClick={() => {
+                            changeScreenAnimation().then(props.handleStateTry);
+                        }}
                         emphasis
                     >Write it down!</Button>
-                </div>
+                </LeftBox>
 
-                <div>
+                <RightBox>
                     <FormContainer
-                        handleStateLogin={() => props.handleStateLogin()}
+                        handleStateLogin={() => {
+                            changeScreenAnimation().then(props.handleStateLogin);
+                        }}
                     />
-                </div>
-            </main>
+                </RightBox>
+            </MainContainer>
         )}
     </React.Fragment>);
 }
