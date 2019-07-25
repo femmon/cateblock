7e772e506a08422d51c5df9562d9e063517bc872
femmon <49613439+femmon@users.noreply.github.com>
2019-07-25 14:24:54 +0700

    Update button style

--- client/src/components/button/index.js
+++ client/src/components/button/index.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-    border: ${props => props.emphasis ? "" : ""};
-    color: ${props => props.emphasis ? "" : ""};
-    background: ${props => props.emphasis ? "" : ""};
+    border: none ${props => props.emphasis ? "" : ""};
+    /*
+     * Corner curves must not overlap, hence the round border:
+     * https://drafts.csswg.org/css-backgrounds/#corner-overlap
+     */
+    border-radius: 9999px;
+    color: white;
+    background: ${props => props.emphasis ? "orange" : "#3BBDFF"};
+    font-weight: bolder;
+    padding: 10px 20px;
 `;
 
 export default Button;
