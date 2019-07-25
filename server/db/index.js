b4b56c1a087b09d7b4dd51863b46353a1a420502
femmon <49613439+femmon@users.noreply.github.com>
2019-07-25 14:22:23 +0700

    Add ability to run SQL query from a file

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
 const mysql = require("mysql");
+const fs = require("fs");
 
 // mysql://user:password@host:port/database
-var layout = /^mysql:\/\/(.+):(.+)@(.+):(\d+)\/(.+)$/.exec(process.env.JAWSDB_URL);
+let layout = /^mysql:\/\/(.+):(.+)@(.+):(\d+)\/(.+)$/.exec(process.env.JAWSDB_URL);
 const pool  = mysql.createPool({
     connectionLimit    : 5,
     database           : layout[5],
@@ -34,5 +35,18 @@ module.exports = {
             });
         });
     },
+    async queryFromFile(path) {
+        let file = await fs.promises.readFile(path, {encoding: "utf-8"});
+        return this.query(file);
+    }
     // postgres project structure have example of automatically kill connection after some time
 }
+
+// https://github.com/mysqljs/mysql#closing-all-the-connections-in-a-pool
+// Call pool.end to end all the connections in the pool, or else the event loop
+// will stay active until the connections are closed by the MySQL server. This
+// is typically done if the pool is used in a script or when trying to
+// gracefully shutdown a server.
+// pool.end(function (err) {
+//     if (err) throw err;
+// });
