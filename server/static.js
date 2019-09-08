92b4e1950665ddf39170855e2f7eecd69fe82a6e
femmon <49613439+femmon@users.noreply.github.com>
2019-09-08 12:23:36 +0700

    Refactor: Reduce gzip handling duplication

--- server/static.js
+++ server/static.js
@@ -4,24 +4,20 @@ const router = express.Router();
 
 const distFolder = path.join(__dirname, "../client/dist");
 
+// Set file name explicitly to match next middleware patter, so that the next
+// middleware can use gzip if needed
+router.get("/", function(req, res, next) {
+    req.url = "/index.html";
+    next();
+});
+
 // Use gzipped file name as name.gz.extension so that I don't have to manually
 // change Content-Type to match each resource (The Content-Type will be gzip
 // and wouldn't display if I send .gz file without manually set Content-Type).
-
-// "*.*" is placed before "/" so that after rewriting the request to "/" into
-// "/index.gz.html", the request wouldn't fall into "*.*" afterward.
 router.get("*.*", function(req, res, next) {
     if (req.get("Accept-Encoding").indexOf("gzip") !== -1) {
         let lastDot = req.url.lastIndexOf(".");
         req.url = req.url.slice(0, lastDot) + ".gz" + req.url.slice(lastDot);
-        res.set("Content-Encoding", "gzip");
-    }
-    next();
-});
-
-router.get("/", function(req, res, next) {
-    if (req.get("Accept-Encoding").indexOf("gzip") !== -1) {
-        req.url = "/index.gz.html";
         res.set("Content-Encoding", "gzip");
     }
     next();
