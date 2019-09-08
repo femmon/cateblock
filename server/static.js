38718088d2d9a13799de93844cf8cf016a4fd892
femmon <49613439+femmon@users.noreply.github.com>
2019-09-08 21:09:11 +0700

    New: Check if server has the gzip version

--- server/static.js
+++ server/static.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const toGzipName = require("../utils/to-gzip-name");
 
 const router = express.Router();
 
 const distFolder = path.join(__dirname, "../client/dist");
+
+const isGzipAvailale = (() => {
+    // Compute once on start up
+    const distFiles = fs.readdirSync(distFolder);
+
+    return async req => {
+        // Check for client support
+        if (req.get("Accept-Encoding").indexOf("gzip") === -1) return false;
+
+        const gzipUrl = toGzipName(req.url);
+
+        // Check if server has the gzip version
+        if (process.env.NODE_ENV !== "development") {
+            // For production, since the client only build assets once before
+            // running the server, we can check against the compute-once file
+            // list `distFiles`
+            // `slice(1)` to remove the initial slash ("/")
+            return distFiles.includes(gzipUrl.slice(1));
+        } else {
+            // For development, since the backend and frontend is refresh
+            // independently, the check will run directly on the file system
+            return await fs.promises.access(distFolder + gzipUrl)
+            .then(() => true)
+            .catch(() => false);
+        }
+    }
+})();
 
 // Set file name explicitly to match next middleware patter, so that the next
 // middleware can use gzip if needed
@@ -16,8 +44,8 @@ router.get("/", function(req, res, next) {
 // Use gzipped file name as name.gz.extension so that I don't have to manually
 // change Content-Type to match each resource (The Content-Type will be gzip
 // and wouldn't display if I send .gz file without manually set Content-Type).
-router.get("*.*", function(req, res, next) {
-    if (req.get("Accept-Encoding").indexOf("gzip") !== -1) {
+router.get("*.*", async function(req, res, next) {
+    if (await isGzipAvailale(req)) {
         req.url = toGzipName(req.url);
         res.set("Content-Encoding", "gzip");
     }
@@ -26,10 +54,10 @@ router.get("*.*", function(req, res, next) {
 
 router.use(express.static(distFolder));
 
-router.get("*", (req, res) => {
+router.get("*", async (req, res) => {
     req.url = "/index.html";
 
-    if (req.get("Accept-Encoding").indexOf("gzip") !== -1) {
+    if (await isGzipAvailale(req)) {
         req.url = toGzipName(req.url);
         res.set("Content-Encoding", "gzip");
     }
