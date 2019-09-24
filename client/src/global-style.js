7f2d94853917a8addc7c8f59230bf0d890720296
femmon <49613439+femmon@users.noreply.github.com>
2019-09-24 22:15:41 +0700

    New: Use `box-sizing: border-box` on every element

--- client/src/global-style.js
+++ client/src/global-style.js
@@ -4,6 +4,10 @@ const GlobalStyle = createGlobalStyle`
     html {
         font-family: "Nunito", sans-serif;
     }
+
+    * {
+        box-sizing: border-box;
+    }
 `;
 
 export default GlobalStyle;
