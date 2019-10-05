682c764d86527331fa7ee14d66ffaef4ef0f722d
femmon <49613439+femmon@users.noreply.github.com>
2019-10-05 23:08:46 +0700

    New: Move static clouds to <Background>
    Centralize the backgrounds for later background animation

--- client/src/components/background/index.js
+++ client/src/components/background/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MovingClouds from "./components/moving-clouds";
+import StaticClouds from "./components/static-clouds";
 
 function Background(props) {
     return (
         <div>
             <MovingClouds />
+            <StaticClouds />
         </div>
     );
 }
