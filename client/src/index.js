852d0f6d890cfa7f8493ef6cd5223641daa32a70
femmon <49613439+femmon@users.noreply.github.com>
2019-12-07 12:40:18 +0700

    Fix: Remove trailing space
    Space is introduced in 201865

--- client/src/index.js
+++ client/src/index.js
@@ -77,7 +77,7 @@ class App extends React.Component {
                         status: "logout",
                         username: null
                     })}
-                />; 
+                />;
                 break;
 
             default:
