0aee696b8c930f363ea3a2e41d42cec52f610fe4
femmon <49613439+femmon@users.noreply.github.com>
2020-02-12 13:29:15 +1300

    Refactor: Move background color of the site to theme.js for reuse

--- client/src/components/background/components/static-clouds/index.js
+++ client/src/components/background/components/static-clouds/index.js
@@ -1,30 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import staticCloudsSrc from "./static-clouds.png";
 
-function StaticClouds(props) {
-    return <div css={`
-        // To replace "background-attachment: fixed" (which doesn't work on
-        // (some) mobile browsers)
-        position: fixed;
-        left: 0;
-        top: 0;
+const StaticClouds = styled.div`
+    // To replace "background-attachment: fixed" (which doesn't work on
+    // (some) mobile browsers)
+    position: fixed;
+    left: 0;
+    top: 0;
 
-        // Use viewport units instead of percentage (100%) so that the
-        // (dis)apprearance of URL bar in mobile browser doesn't change the
-        // size of this ::before (This behavior works on Safari and Chrome
-        // for IOS, doesn't on Firefox for IOS)
-        width: 100vw;
-        height: 100vh;
+    // Use viewport units instead of percentage (100%) so that the
+    // (dis)apprearance of URL bar in mobile browser doesn't change the
+    // size of this ::before (This behavior works on Safari and Chrome
+    // for IOS, doesn't on Firefox for IOS)
+    width: 100vw;
+    height: 100vh;
 
-        background-color: hsl(194.6, 61.2%, 86.9%);
-        // The image flickers in development build, relates to this
-        // https://github.com/styled-components/styled-components/issues/1593
-        background-image: url(${staticCloudsSrc});
-        background-repeat: no-repeat;
-        background-size: cover;
-        content: "";
-        z-index: -2;
-    `}/>;
-}
+    background-color: ${props => props.theme.blueBackground};
+    // The image flickers in development build, relates to this
+    // https://github.com/styled-components/styled-components/issues/1593
+    background-image: url(${staticCloudsSrc});
+    background-repeat: no-repeat;
+    background-size: cover;
+    content: "";
+    z-index: -2;
+`;
 
 export default StaticClouds;
