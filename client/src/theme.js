d5d5f60116d28919111e8f14eaca5a4a2d6ff6fa
femmon <49613439+femmon@users.noreply.github.com>
2020-02-12 13:46:39 +1300

    Refactor: Move background color of the site to theme.js for reuse

--- client/src/theme.js
+++ client/src/theme.js
@@ -4,7 +4,8 @@ const theme = {
         position: absolute;
         left: -9999px;
         top: -9999px;
-    `
+    `,
+    blueBackground: "hsl(194.6, 61.2%, 86.9%)"
 };
 
 export default theme;
